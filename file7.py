49bb68d0adf97fe899aeb6895f615a2fc876b096
Kids Koding <68204671+teamdabbers2@users.noreply.github.com>
2020-10-26 13:09:44 -0500

    Create file7.py

---
+++ file7.py
@@ -0,0 +1,16 @@
+#you can do this code
+food = ["Ice Cream", "Cake", "Cookie"]
+ 
+food.pop(1)
+ 
+print(food)
+
+#or this code
+food = ["Ice Cream", "Cake", "Cookie"]
+ 
+food.remove("Cake")
+ 
+print(food)
+
+
+#the pop method requires an index and the remove method requires the value. Either way, they both remove items from the arrays
