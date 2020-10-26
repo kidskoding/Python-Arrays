663a4b08e33cbfc8f8a53a8de0cb1c2cda6c63c5
Kids Koding <68204671+teamdabbers2@users.noreply.github.com>
2020-10-26 13:10:56 -0500

    Update file1.py

--- file1.py
+++ file1.py
@@ -3,3 +3,5 @@ food = ["Ice Cream", "Cake", "Cookie"]
 x = food[0]
  
 print(x)
+
+#this allows us to access a value from the array. In this case, I want to access the first item because 0 means the first item in the list. 1 means the second item in the list
