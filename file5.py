f52f52f824b181806c889d0893024319c39d694f
Kids Koding <68204671+teamdabbers2@users.noreply.github.com>
2020-10-26 13:16:40 -0500

    Update file5.py

--- file5.py
+++ file5.py
@@ -2,3 +2,6 @@ food = ["Ice Cream", "Cake", "Cookie"]
  
 for x in food:
     print(x)
+
+#we implement for loops into arrays. This will allow the terminal to loop through all values that are in the array. In other words, it will list all the values
+#that are in the array
