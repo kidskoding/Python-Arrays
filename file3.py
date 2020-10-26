7872c98904abb39d89c7b15cb22c1324bff6d14c
Kids Koding <68204671+teamdabbers2@users.noreply.github.com>
2020-10-26 13:13:57 -0500

    Update file3.py

--- file3.py
+++ file3.py
@@ -3,3 +3,8 @@ food = ["Ice Cream", "Cake", "Cookie"]
 food[0] = "Scones"
  
 print(food)
+
+#the terminal will substitute Ice Cream for Scones
+#I am asking the terminal to pick the 1st value in the array, which is Ice Cream
+#and I am now saying that the index food[0] will now equal Scones
+#therefore the terminal will replace Ice Cream with Scones
