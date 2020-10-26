c678f5e72580054e648866cbe54966c825711a25
Kids Koding <68204671+teamdabbers2@users.noreply.github.com>
2020-10-26 13:14:38 -0500

    Update file4.py

--- file4.py
+++ file4.py
@@ -3,3 +3,6 @@ food = ["Ice Cream", "Cake", "Cookie"]
 x = len(food)
  
 print(x)
+
+#by using the len function, we ask the terminal how many values are in the array
+#the terminal will display 3 because there are 3 values in the array
