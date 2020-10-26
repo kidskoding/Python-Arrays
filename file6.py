828b2d28f7bd548319f353e404b1347c62b4b755
Kids Koding <68204671+teamdabbers2@users.noreply.github.com>
2020-10-26 13:17:40 -0500

    Update file6.py

--- file6.py
+++ file6.py
@@ -3,3 +3,5 @@ food = ["Ice Cream", "Cake", "Cookie"]
 food.append("Scones")
  
 print(food)
+
+#we are adding the value Scones into the array by using the .append function. This will add it to the end of the array
